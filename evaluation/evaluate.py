4bb14fbfab326f2ede8c5af862f3ae8bf770f250
Carlos de Vera Sanz <deverasanzcarlos@gmail.com>
2025-06-09 07:38:19 +0200

    Update evaluate.py

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -606,7 +606,7 @@ class AgentEvaluator:
                 ]
                 
                 # Evaluar solo los casos evaluables
-                results = evaluate(cache_config=CacheConfig(write_cache=False, use_cache=False),
+                results = (cache_config=CacheConfig(write_cache=False, use_cache=False),
                                    error_config=ErrorConfig(ignore_errors=False),
                                    async_config=AsyncConfig(run_async=False),
                                    test_cases=casos_evaluables, metrics=metrics)
@@ -665,7 +665,7 @@ class AgentEvaluator:
                 ]
                 
                 # Evaluar solo los casos evaluables
-                results = evaluate(cache_config=CacheConfig(write_cache=True, use_cache=True),
+                results = evaluate(cache_config=CacheConfig(write_cache=False, use_cache=False),
                                    error_config=ErrorConfig(ignore_errors=False),
                                    async_config=AsyncConfig(run_async=False),
                                    test_cases=casos_evaluables, metrics=metrics)
